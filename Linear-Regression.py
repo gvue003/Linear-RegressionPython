3d906532e989862cf280cd34bd0b46ab91408ea0
Geng Cha Vue <51110353+gvue003@users.noreply.github.com>
2023-09-30 17:30:09 -0500

    Update Linear-Regression.py

--- Linear-Regression.py
+++ Linear-Regression.py
@@ -12,18 +12,15 @@ class LinearRegression:
     def fit(self, X, y):
         n_samples, n_features = X.shape
 
-        # init parameters
         self.weights = np.zeros(n_features)
         self.bias = 0
-
-        # gradient descent
+        
         for _ in range(self.n_iters):
             linear_model = np.dot(X, self.weights) + self.bias
 
             dw = (2/n_samples) * np.dot(X.T, (linear_model - y))
             db = (2/n_samples) * np.sum(linear_model - y)
 
-            # update parameters
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
 
@@ -31,17 +28,14 @@ class LinearRegression:
         linear_model = np.dot(X, self.weights) + self.bias
         return linear_model
 
-# preparing data
 X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
 y = np.array([2, 4, 6, 8, 10])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-# training the model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# predictions
 y_pred = model.predict(X_test)
 
 # results
